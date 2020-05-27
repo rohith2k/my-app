f32adf6e1bfc54621ee035905b972a5538cbc498
rohith2k <49037926+rohith2k@users.noreply.github.com>
2020-05-27 13:10:19 +0530

    changed Comp1

--- src/Comp1.js
+++ src/Comp1.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -7,58 +7,63 @@ import FormControl from 'react-bootstrap/FormControl';
 import {questions} from './Data';
 
 var msgs=questions.worflow_template;//messages to be displayed on the system side
-
-var output=msgs.map((msg)=>{
-  var heading = <div class="questionControl">{msg.question_text}</div>;
-  var inner_output;
-  if(msg.selection_type==="Single-Select")
-  {
-      inner_output =msg.options.map((option)=>{
-      return (<button class="list-item-question list-group-item list-group-item-light list-group-item-action" style={{outline:"invert"}}>{option.name}</button>);
-    });
-    inner_output=<div className="buttonControl">{inner_output}</div>;
-  }
-  else if(msg.selection_type==="Number")
-  {
-    inner_output=(
-    <div className="inputControl">
-    <InputGroup className="mb-3">
-    <FormControl
-      aria-describedby="basic-addon2"
-    />
-    <InputGroup.Append>
-      <Button variant="outline-secondary">Next</Button>
-    </InputGroup.Append>
-    </InputGroup>
-    </div>);
-  }
-  else if(msg.selection_type==="Multi-Select")
-  {
-      inner_output =msg.options.map((option)=>{
-      return (
-          <div className="mb-3">
-            <Form.Check 
-              type='checkbox'
-              label={option.name}
-            />
-          </div>);
-      });
-    inner_output.push(<Button variant="outline-secondary">Next</Button>);
-    inner_output=<Form>{inner_output}</Form>;
-  }
-  else ;
-  return ([heading,<br/>,inner_output,<br/>]);
-}
-
-);
-
 //returning messages in the form of ListGroup
-const Comp1=()=>{    
+const Comp1=()=>{
+  const [count,setCount] = useState(1); //displays 1st question initially
+  //setDisplay(display.push(ordered_output[0]));   
       return(
+        <>
         <div className="sys_msgs">
-        <ListGroup >{output}</ListGroup>
+        <ListGroup >{
+        msgs.slice(0,count).map((msg,index)=>{
+          if(index<count)
+          {
+            var heading = <div class="questionControl">{msg.question_text}</div>;
+            var inner_output;
+            if(msg.selection_type==="Single-Select")
+            {
+                inner_output =msg.options.map((option)=>{
+                return (<button type="button" class="list-item-question list-group-item list-group-item-light list-group-item-action" onClick={() => setCount(count + 1)} style={{outline:"invert"}}>{option.name}</button>);
+              });
+              inner_output=<div className="buttonControl">{inner_output}</div>;
+            }
+            else if(msg.selection_type==="Number")
+            {
+              inner_output=(
+              <div className="inputControl">
+              <InputGroup className="mb-3">
+              <FormControl
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" onClick={() => setCount(count + 1)}>Next</Button>
+              </InputGroup.Append>
+              </InputGroup>
+              </div>);
+            }
+            else if(msg.selection_type==="Multi-Select")
+            {
+                inner_output =msg.options.map((option)=>{
+                return (
+                    <div className="mb-3">
+                      <Form.Check 
+                        type='checkbox'
+                        label={option.name}
+                      />
+                    </div>);
+                });
+              inner_output.push(<Button variant="outline-secondary" onClick={() => setCount(count + 1)}>Next</Button>);
+              inner_output=<Form>{inner_output}</Form>;
+            }
+            else ;
+          }
+          return ([heading,<br/>,inner_output,<br/>]);
+        }
+        )}
+        </ListGroup>
         </div>
-        );
+        </>
+      );
   
   }
 export default Comp1;
